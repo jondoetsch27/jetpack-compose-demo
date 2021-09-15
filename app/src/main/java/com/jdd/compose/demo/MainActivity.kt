961d7e86594a39c02a559de82a37d03844995d89
jondoetsch27 <jd@enfuse.io>
2021-09-15 14:40:06 -0700

    09/15 - image and text formatting @jondoetsch27

--- app/src/main/java/com/jdd/compose/demo/MainActivity.kt
+++ app/src/main/java/com/jdd/compose/demo/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jdd.compose.demo.ui.theme.JetpackComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,15 +46,20 @@ data class PlayerMessage(
 
 @Composable
 fun PlayerCard(playerMessage: PlayerMessage) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.trevor_lawrence_profile_picture),
-            contentDescription = "Player Profile Picture"
+            contentDescription = "Player Profile Picture",
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = playerMessage.playerFirstName)
-            Text(text = playerMessage.playerLastName)
-            Text(text = playerMessage.playerNumber)
+            Text(text = playerMessage.playerFirstName, color = MaterialTheme.colors.secondaryVariant)
+            Text(text = playerMessage.playerLastName, color = MaterialTheme.colors.secondaryVariant)
+            Text(text = playerMessage.playerNumber, color = MaterialTheme.colors.secondaryVariant)
         }
     }
 }
